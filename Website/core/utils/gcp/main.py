ab75b1e8ad5d92bf64f3ae2f0b1d5b2c529f1582
Kaleb Bishop <Kalebwbishop@gmail.com>
2025-04-23 17:20:05 -0400

    Build returns url

--- Website/core/utils/gcp/main.py
+++ Website/core/utils/gcp/main.py
@@ -211,7 +211,7 @@ class GCPUtils:
         except Exception as e:
             print(f"An error occurred: {e}")
 
-    def deploy_service(self, stack_id: str, image_name: str, layer: str, env_vars: dict | None = None, **kwargs) -> None:
+    def deploy_service(self, stack_id: str, image_name: str, layer: str, env_vars: dict | None = None, **kwargs) -> str:
         service_name = f"{layer.lower()}-{stack_id.lower()}"
 
         port = kwargs.get("port", 8080) if kwargs else 8080
@@ -270,7 +270,7 @@ class GCPUtils:
 
             if response is None:
                 print(f"Error creating build: {response}")
-                return
+                raise Exception("Error creating build")
 
             response.get("metadata", {})
             metadata = response.get("metadata", {})
@@ -279,7 +279,7 @@ class GCPUtils:
 
             if not build_id:
                 print(f"Error creating build: {response}")
-                return
+                raise Exception("Error creating build")
 
             print(f"Build submitted with ID: {build_id}")
 
@@ -314,6 +314,7 @@ class GCPUtils:
                 print(
                     f"Build log URL: https://console.cloud.google.com/cloud-build/builds/{build_id}?project={project_id}"
                 )
+                return self.get_service_endpoint(service_name)
             else:
                 print(f"Build failed with status: {status}")
                 print(
@@ -344,6 +345,20 @@ class GCPUtils:
 
         return {"status": "success", "data": cleaned_services}
 
+    def get_service_endpoint(self, service_name: str) -> str:
+        url = f"https://run.googleapis.com/v2/projects/deploy-box/locations/us-central1/services/{service_name}"
+        response = self.__request_helper(url)
+
+        if response is None:
+            raise Exception("Service not found")
+
+        service = response.get("uri", "")
+
+        if not service:
+            raise Exception("Service not found")
+
+        print(f"Service URL: {service}")
+        return service
 
     # TODO: This currently only grabs the latest logs, not a stream
     def stream_logs(
@@ -412,13 +427,15 @@ class GCPUtils:
 if __name__ == "__main__":
     gcp_wrapper = GCPUtils()
 
-    backend_image = "gcr.io/deploy-box/mern-backend"
-    backend_url = gcp_wrapper.deploy_service(
-            "testing-1234",
-            backend_image,
-            "backend",
-            {"MONGO_URI": "mongodb+srv://deployBoxUser-e9c9afbc1c0942c8:f45bdc78db0a@cluster0.yjaoi.mongodb.net/db-e9c9afbc1c0942c8?retryWrites=true&w=majority&appName=Cluster0"},
-            port=5001
-        )
+    print(gcp_wrapper.get_service_endpoint("backend-9a77369a03984b9a"))
+
+    # backend_image = "gcr.io/deploy-box/mern-backend"
+    # backend_url = gcp_wrapper.deploy_service(
+    #         "testing-1234",
+    #         backend_image,
+    #         "backend",
+    #         {"MONGO_URI": "mongodb+srv://deployBoxUser-e9c9afbc1c0942c8:f45bdc78db0a@cluster0.yjaoi.mongodb.net/db-e9c9afbc1c0942c8?retryWrites=true&w=majority&appName=Cluster0"},
+    #         port=5001
+    #     )
 
 
