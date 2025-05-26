4061f90a0274192678d513ded6db18055d5fad0e
WernkeJD <Jacob.Wernke@yahoo.com>
2025-05-25 20:50:44 -0400

    basics working

--- stacks/MERN Pro/backend/middleware/websocket_middleware.js
+++ stacks/MERN Pro/backend/middleware/websocket_middleware.js
@@ -6,27 +6,35 @@ const port = process.env.PORT || 5500;
 const app = express();
 
 const expressServer = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 const io = new Server(expressServer, {
-    cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://127.0.0.1:5500", "http://localhost:5500"],
-        methods: ["GET", "POST"]
-    },
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? false
+        : [
+            "http://127.0.0.1:5500",
+            "http://localhost:5500",
+            "http://127.0.0.1:3000",
+            "http://localhost:3000",
+          ],
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log(`User ${socket.id} connected`);
-    
-    socket.on("message", (data) => {
-        console.log(`Message received: ${data}`);
-        io.emit('message', `${socket.id.slice(0,4)}: ${data}`);
-    });
+  console.log(`User ${socket.id} connected`);
 
-    socket.on("disconnect", () => {
-        console.log(`User ${socket.id} disconnected`);
-    });
+  socket.on("message", (data) => {
+    console.log(`Message received: ${data}`);
+    io.emit("message", `${socket.id.slice(0, 4)}: ${data}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} disconnected`);
+  });
 });
 
 module.exports = { io, app, expressServer };
