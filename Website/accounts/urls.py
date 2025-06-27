4093e93263e29fb019f5f27f3b1feabdd990ad25
WernkeJD <Jacob.Wernke@yahoo.com>
2025-06-26 21:29:48 -0400

    added ability to add existing members

--- Website/accounts/urls.py
+++ Website/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SignupAPIView, OAuthPasswordLoginView, OAuthClientCredentialsView, M2MProtectedView
 
 urlpatterns = [
-    path("signup/", SignupAPIView.as_view(), name="api-signup"),
+    path("signup/", SignupAPIView.as_view(), name="signup"),
     path("login/", OAuthPasswordLoginView.as_view(), name="api-login"),
     path("oauth/m2m/", OAuthClientCredentialsView.as_view(), name="oauth-m2m"),
     path("m2m/protected/", M2MProtectedView.as_view(), name="m2m-protected"),
