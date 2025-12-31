e65c5519b43110934a66b8ababa7483d22858eda
kalebwbishop <kalebwbishop@gmail.com>
2025-12-30 22:00:47 -0500

    Update apps.py

--- Website/marketplace/apps.py
+++ Website/marketplace/apps.py
@@ -1,12 +1,5 @@
 from django.apps import AppConfig
 
-
-<<<<<<<< HEAD:Website/marketplace/apps.py
 class MarketplaceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'marketplace'
-========
-class DeployBoxApisConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'deploy_box_apis'
->>>>>>>> main:Website/deploy_box_apis/apps.py
